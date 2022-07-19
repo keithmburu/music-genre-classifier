bb7966bd14f7bd5078681f376377d6dc7107c51c
kmburu <keithmburu1@gmail.com>
2022-07-19 12:57:01 +0300

    comments

--- Partition.py
+++ Partition.py
@@ -1,4 +1,12 @@
-#borrows from lab 5
+"""
+CS260 Project
+Example and partition classes with labels
+Adapted from Lab 5 (Sara Mathieson)
+Author: Keith Mburu
+Author: Matt Gusdorff
+Date: 12/17/2021
+"""
+
 class Example:
 
     def __init__(self, features, label):
@@ -18,3 +26,10 @@ class Partition:
 
         # number of classes
         self.K = K
+
+        temp = []
+        for example in self.data:
+            if example.label not in temp:
+                temp.append(example.label)
+
+        self.labels = temp
